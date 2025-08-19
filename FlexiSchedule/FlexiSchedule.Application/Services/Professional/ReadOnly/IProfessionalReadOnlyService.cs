22ae11589d2f1ed14537b385ce22c9aae5b91a60
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-08-19 16:48:19 -0300

    style: remove unnecessary using

--- FlexiSchedule/FlexiSchedule.Application/Services/Professional/ReadOnly/IProfessionalReadOnlyService.cs
+++ FlexiSchedule/FlexiSchedule.Application/Services/Professional/ReadOnly/IProfessionalReadOnlyService.cs
@@ -1,6 +1,4 @@
-﻿using FlexiSchedule.Application.Models.ViewModels;
-
-namespace FlexiSchedule.Application.Services.Professional.ReadOnly;
+﻿namespace FlexiSchedule.Application.Services.Professional.ReadOnly;
 public interface IProfessionalReadOnlyService
 {
     Task<IEnumerable<ProfessionalViewModel>> GetAllAsync(CancellationToken cancellationToken = default);
