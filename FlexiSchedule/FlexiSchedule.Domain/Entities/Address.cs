af9a4f7cc970cda59e09a6d61f6950df6e24d5d7
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-09-10 20:14:33 -0300

    feat: implements client and address entities

---
+++ FlexiSchedule/FlexiSchedule.Domain/Entities/Address.cs
@@ -0,0 +1,29 @@
+﻿namespace FlexiSchedule.Domain.Entities;
+public class Address : BaseEntity
+{
+    public string Street { get; private set; }
+    public int Number { get; private set; }
+    public string District { get; private set; }
+    public string City { get; private set; }
+    public string State { get; private set; }
+    public string Country { get; private set; }
+    public string ZipCode { get; private set; }
+    public string Complement { get; private set; }
+
+    //relationships
+    public Guid ClientId { get; private set; }
+
+    public Address(string street, int number, string district, string city, string state, string country, 
+        string zipCode, string complement, Guid clientId)
+    {
+        Street = street;
+        Number = number;
+        District = district;
+        City = city;
+        State = state;
+        Country = country;
+        ZipCode = zipCode;
+        Complement = complement;
+        ClientId = clientId;
+    }
+}
---
+++ FlexiSchedule/FlexiSchedule.Domain/Entities/Client.cs
@@ -0,0 +1,19 @@
+﻿namespace FlexiSchedule.Domain.Entities;
+public class Client : BaseEntity
+{
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Phone { get; private set; }
+
+    //relationships
+    public Guid ProfessionalId { get; private set; }
+    public virtual Address? Address { get; private set; }
+
+    public Client(string name, string email, string phone, Guid professionalId)
+    {
+        Name = name;
+        Email = email;
+        Phone = phone;
+        ProfessionalId = professionalId;
+    }
+}
