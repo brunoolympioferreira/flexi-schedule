b7e6d3eee63c83750217668732fa74c03b27b667
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-09-23 20:08:29 -0300

    adjust endpoint

--- FlexiSchedule/FlexiSchedule.API/Controllers/ClientsController.cs
+++ FlexiSchedule/FlexiSchedule.API/Controllers/ClientsController.cs
@@ -17,7 +17,7 @@ public class ClientsController : ControllerBase
         return CreatedAtAction(nameof(Create), new { id = clientId }, null);
     }
 
-    [HttpGet("professionals/{professionalId:guid}")]
+    [HttpGet("professional/{professionalId:guid}")]
     public  IActionResult GetAllByProfessionalId(
         [FromServices] IClientReadOnlyService clientReadOnlyService,
         [FromRoute] Guid professionalId)
